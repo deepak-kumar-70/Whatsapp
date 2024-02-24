7a9894cfd7533796c32f96eb62c65785f133688d
deepak-kumar-70 <141209444+deepak-kumar-70@users.noreply.github.com>
2024-02-24 12:31:34 +0530

    first commit

---
+++ src/Store/slice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {};
+const Slice = createSlice({
+  name: "",
+  initialState,
+  reducers: {},
+});
+export default Slice.reducer;
